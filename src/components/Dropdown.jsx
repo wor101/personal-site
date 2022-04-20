50d42a72c78adcc88a7576ff00703a9effa582b8
Will <wor101@gmail.com>
2022-04-20 17:01:24 -0400

    added reactive dropdown

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const navLinkFormatting = "flex flex-col justify-center px-3 h-14 bg-black font-semibold text-white uppercase text-center hover:bg-slate-600"
+
+const NavLink = ({ href, text, toggleDropdown}) => {
+  return (
+    <a href={href} className={navLinkFormatting} onClick={toggleDropdown}>{text}</a>
+  )
+}
+
+const Dropdown = ({toggleDropdown}) => {
+  return (
+    <div className="fixed top-14 left-0 right-0 z-30">
+      <NavLink href="#home" text="Home" toggleDropdown={toggleDropdown} />
+      <NavLink href="#about" text="About" toggleDropdown={toggleDropdown} />
+      <NavLink href="#contact" text="Contact" toggleDropdown={toggleDropdown} />
+      <NavLink href="#projects" text="Projects" toggleDropdown={toggleDropdown} />
+    </div>
+  )
+}
+
+export default Dropdown
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const navLinkFormatting = "flex flex-col justify-center px-3 font-semibold text-white uppercase hover:bg-slate-600"
+
+const NavLink = ({ href, text}) => {
+  return (
+    <a href={href} className={"hidden md:" + navLinkFormatting}>{text}</a>
+  )
+}
+
+const MenuIcon = ({ toggleDropdown }) => {
+  return (
+    <button onClick={toggleDropdown} className="flex flex-col justify-center md:hidden">
+      <svg className="w-6 h-6" fill="none" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
+    </button>
+  )
+}
+
+const Navbar = ({ toggleDropdown }) => {
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-30 bg-black h-14">
+      <div className="flex justify-between max-w-7xl mx-auto px-7 h-14">
+        <a href="#home" className={navLinkFormatting}>Will Rossen</a>
+        <div className="flex justify-end">
+          <NavLink href="#home" text="Home" />
+          <NavLink href="#about" text="About" />
+          <NavLink href="#contact" text="Contact" />
+          <NavLink href="#projects" text="Projects" />
+          <MenuIcon toggleDropdown={toggleDropdown} />
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export { 
+  Navbar,
+  NavLink
+}
