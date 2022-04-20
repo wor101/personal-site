164fa6a398adfbf55f0f90e0b2ea3a1b1b2bebfe
Will <wor101@gmail.com>
2022-04-20 11:27:45 -0400

    stopped bleed through of background into overlaying text

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="bg-black">
-      <div className="absolute top-0 left-0 right-0 bg-mackinaw blur-[.10rem] bg-center bg-no-repeat bg-cover bg-fixed opacity-40 min-h-screen"></div>
-      <span className="bg-green h-96">
-        <section className="flex flex-col min-h-screen items-strectch justify-between text-center">
-          <h1 className="text-xl text-red-900">Will Rossen</h1>
-        </section>
-      </span>
-      <p>stuff</p>
+        <section className="before:absolute before:top-0 before:left-0 before:right-0 before:bottom-0 before:bg-mackinaw before:bg-center before:bg-no-repeat before:bg-cover before:bg-fixed before:opacity-40 before:min-h-screen flex flex-col min-h-screen items-strectch justify-between text-center ">
+            <h1 className="relative text-8xl text-white opacity-100">Will Rossen</h1>
+          </section>
+        <p className="text-white">stuff</p>
     </div>
   );
 }
@@ -38,5 +35,13 @@ export default App;
         <p className="text-white">stuff</p>
     </div>
 
+
+        <div className="bg-black">
+      <span className="absolute top-0 left-0 right-0 bg-mackinaw blur-[.10rem] bg-center bg-no-repeat bg-cover bg-fixed opacity-40 min-h-screen"></span>
+      <section className="flex flex-col min-h-screen items-strectch justify-between text-center">
+        <h1 className="text-8xl text-white">Will Rossen</h1>
+      </section>
+      <p>stuff</p>
+    </div>
 
 */
