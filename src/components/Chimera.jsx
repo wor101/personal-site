c945490f7a815f78e6fc191c670d12c5eacf061b
Will <wor101@gmail.com>
2022-04-26 14:08:33 -0400

    added Shadows to logo and diagram in Chimera

--- src/components/Chimera.jsx
+++ src/components/Chimera.jsx
@@ -7,7 +7,7 @@ const Chimera = () => {
   return (
     <div id="chimera" className="flex place-content-center min-h-screen bg-white">
       <div className="flex flex-wrap md:flex-nowrap place-content-center items-center max-w-7xl px-7 py-7 overflow-hidden">
-        <div className="flex flex-col bg-[#66AC9A] rounded-full">
+        <div className="flex flex-col bg-[#66AC9A] rounded-full shadow-2xl">
           <a href="https://chimera-deploy.dev/">
             <figure>
               <img src={chimeraLogo} alt="chimera logo"></img>
@@ -20,7 +20,7 @@ const Chimera = () => {
           <p className="text-xl mx-6">It allows software development teams to safely and easily deploy new versions of their microservices by taking advantage of the features provided by their existing service mesh</p>
           <a href="https://chimera-deploy.dev/">
             <figure className="flex place-content-center">
-              <img src={chimeraDiagram} alt="chimera logo" className="max-h-80"></img>
+              <img src={chimeraDiagram} alt="chimera logo" className="max-h-80 shadow-2xl"></img>
             </figure>
           </a>
           <div className="flex place-content-center text-xl">
