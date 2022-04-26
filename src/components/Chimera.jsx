be04ecb193d2500d88621a234aa040a17570322f
Will <wor101@gmail.com>
2022-04-26 09:37:12 -0400

    adding new logo

---
+++ src/components/Chimera.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import chimeraLogo from  '../images/Chimera_graphic_color.png'
+import chimeraDiagram from '../images/Canary-Full-Diagram.png'
+
+const Chimera = () => {
+  return (
+    <div className="flex place-content-center min-h-screen bg-white">
+      <div className="flex flex-wrap md:flex-nowrap place-content-center items-center max-w-7xl px-7 py-7">
+        <div className="flex flex-col">
+          <figure>
+            <img src={chimeraLogo} alt="chimera logo"></img>
+          </figure>
+        </div>
+        <div className="flex flex-col px-6 py-6 space-y-4">
+          <h2 className="font-koulen text-9xl text-center">CHIMERA</h2>
+          <p className="text-lg">An open-source tool for performing automated canary deployments of containerized microservices.</p>
+          <p className="text-lg">It allows software development teams to safely and easily deploy new versions of their microservices by taking advantage of the features provided by their existing service mesh</p>
+          <div className="flex place-content-center text-xl">
+            <a href="https://chimera-deploy.dev/" target="_blank" rel="noreferrer" className="grow max-w-sm py-1 mx-1 border border-black rounded-full uppercase text-center hover:bg-black hover:text-white">Write Up</a>
+            {/* <a href="https://chimera-deploy.dev/presentation" target="_blank" rel="noreferrer" className="grow max-w-sm py-1 mx-1 border border-black rounded-full uppercase text-center hover:bg-black hover:text-white">Presentation</a> */}
+          </div>
+        </div>
+
+      </div>
+      {/* <div id="chimera" className="grow flex flex-col max-w-7xl px-7 place-content-center items-center space-y-2">
+        CHIMERA
+      </div> */}
+    </div>
+  )
+}
+
+export default Chimera
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import About from '../components/About'
+import Chimera from '../components/Chimera'
 import Contact from '../components/Contact'
+
 
 const Home = () => {
   return (
     <>
       <Hero />
       <About />
+      <Chimera />
       <Contact />
     </>
   )
